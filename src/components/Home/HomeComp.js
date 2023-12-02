be526b0c9df527b340a95050d410267352a40daf
Ion Stici <ionsticidev@gmail.com>
2023-12-02 09:49:22 +0200

    Add filter jobs logic

--- src/components/Home/HomeComp.js
+++ src/components/Home/HomeComp.js
@@ -1,13 +1,20 @@
 import Jobs from "./Jobs";
 import FilterBar from "./FilterBar";
+import { useState } from "react";
 
 function HomeComp({ jobs }) {
+  const [jobFilter, setJobFilter] = useState({
+    title: "",
+    loc: "",
+    fullTime: false,
+  });
+
   const locations = [...new Set(jobs.map(({ location }) => location))];
 
   return (
     <>
-      <FilterBar locations={locations} />
-      <Jobs jobs={jobs} />
+      <FilterBar locations={locations} setJobFilter={setJobFilter} />
+      <Jobs jobs={jobs} jobFilter={jobFilter} />
     </>
   );
 }
--- src/components/Home/Jobs.js
+++ src/components/Home/Jobs.js
@@ -1,9 +1,39 @@
 import styles from "./../../styles/Jobs.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Job from "./Job";
 import Button from "../ui/Button";
 
-function Jobs({ jobs }) {
+function Jobs({ jobs: allJobs, jobFilter }) {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const filteredJobs = allJobs.filter((job) => {
+      let title, loc, fullTime;
+
+      if (jobFilter.title) {
+        title = job.position.toLowerCase().includes(jobFilter.title);
+      } else {
+        title = true;
+      }
+
+      if (jobFilter.loc) {
+        loc = job.location === jobFilter.loc;
+      } else {
+        loc = true;
+      }
+
+      if (jobFilter.fullTime) {
+        fullTime = job.contract === "Full Time";
+      } else {
+        fullTime = true;
+      }
+
+      if (title && loc && fullTime) return job;
+    });
+
+    setJobs(filteredJobs);
+  }, []);
+
   const [jobsNum, setJobsNum] = useState(12);
   const jobsOnPage = jobs.slice(0, jobsNum);
 
@@ -12,7 +42,6 @@ function Jobs({ jobs }) {
       setJobsNum((prev) => prev - 3);
       return;
     }
-
     setJobsNum((prev) => prev + 3);
   }
 
@@ -24,12 +53,12 @@ function Jobs({ jobs }) {
         })}
       </section>
 
-      {
+      {!(jobsOnPage.length < jobsNum) && (
         <Button
           onClick={handleLoadMore}
           text={!(jobsNum === jobs.length) ? "Load More" : "Show Less"}
         />
-      }
+      )}
     </>
   );
 }
