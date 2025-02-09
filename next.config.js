ea5d6ec133af864a804e8f82d7b187a775393174
Ion Stici <ionsticidev@gmail.com>
2025-02-09 10:47:07 +0200

    Prefix the base path for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/devjobs-web-app",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/data/assets.js
+++ src/data/assets.js
@@ -1,21 +1,23 @@
+import { basePath } from "./constants";
+
 export const assets = {
   mobile: {
-    bgPatternHeader: "/assets/mobile/bg-pattern-header.svg",
-    bgPatternFooter: "/assets/mobile/bg-pattern-detail-footer.svg",
-    iconFilter: "/assets/mobile/icon-filter.svg",
+    bgPatternHeader: basePath + "/assets/mobile/bg-pattern-header.svg",
+    bgPatternFooter: basePath + "/assets/mobile/bg-pattern-detail-footer.svg",
+    iconFilter: basePath + "/assets/mobile/icon-filter.svg",
   },
   tablet: {
-    bgPatternHeader: "/assets/tablet/bg-pattern-header.svg",
+    bgPatternHeader: basePath + "/assets/tablet/bg-pattern-header.svg",
   },
   desktop: {
-    bgPatternHeader: "/assets/desktop/bg-pattern-header.svg",
-    bgPatternFooter: "/assets/desktop/bg-pattern-detail-footer.svg",
-    iconCheck: "/assets/desktop/icon-check.svg",
-    iconLocation: "/assets/desktop/icon-location.svg",
-    iconMoon: "/assets/desktop/icon-moon.svg",
-    iconSearch: "/assets/desktop/icon-search.svg",
-    iconSearchWhite: "/assets/desktop/icon-search-white.svg",
-    iconSun: "/assets/desktop/icon-sun.svg",
-    logo: "/assets/desktop/logo.svg",
+    bgPatternHeader: basePath + "/assets/desktop/bg-pattern-header.svg",
+    bgPatternFooter: basePath + "/assets/desktop/bg-pattern-detail-footer.svg",
+    iconCheck: basePath + "/assets/desktop/icon-check.svg",
+    iconLocation: basePath + "/assets/desktop/icon-location.svg",
+    iconMoon: basePath + "/assets/desktop/icon-moon.svg",
+    iconSearch: basePath + "/assets/desktop/icon-search.svg",
+    iconSearchWhite: basePath + "/assets/desktop/icon-search-white.svg",
+    iconSun: basePath + "/assets/desktop/icon-sun.svg",
+    logo: basePath + "/assets/desktop/logo.svg",
   },
 };
